64ae90e9b9cdea8b9e911457609d12b264409751
Matuyuhi <bird9.yuhi@gmail.com>
2023-07-17 00:34:09 +0900

    nullを足してもエラーにならないように修正

--- Scripts/RunTime/TransitionSpec.cs
+++ Scripts/RunTime/TransitionSpec.cs
@@ -27,7 +27,70 @@ namespace AnimationPro.RunTime
             TransitionSpec b
         )
         {
-            return new TransitionSpec(a.rotate, a.position + b.position, a.alpha + b.alpha);
+            return new TransitionSpec(
+            NullableSum(a.rotate, b.rotate),
+            NullableSum(a.position, b.position), 
+            NullableSum(a.alpha, b.alpha)
+            );
+        }
+        public static Vector3? NullableSum(Vector3? value1, Vector3? value2)
+        {
+            if (!value1.HasValue && !value2.HasValue)
+            {
+                return null;
+            }
+            else if (!value1.HasValue)
+            {
+                return value2.Value;
+            }
+            else if (!value2.HasValue)
+            {
+                return value1.Value;
+            }
+            else
+            {
+                return value1.Value + value2.Value;
+            }
+        }
+
+        public static Quaternion? NullableSum(Quaternion? value1, Quaternion? value2)
+        {
+            if (!value1.HasValue && !value2.HasValue)
+            {
+                return null;
+            }
+            else if (!value1.HasValue)
+            {
+                return value2.Value;
+            }
+            else if (!value2.HasValue)
+            {
+                return value1.Value;
+            }
+            else
+            {
+                return value1.Value * value2.Value; // Quaternionは通常、乗算で結合します。
+            }
+        }
+
+        public static float? NullableSum(float? value1, float? value2)
+        {
+            if (!value1.HasValue && !value2.HasValue)
+            {
+                return null;
+            }
+            else if (!value1.HasValue)
+            {
+                return value2.Value;
+            }
+            else if (!value2.HasValue)
+            {
+                return value1.Value;
+            }
+            else
+            {
+                return value1.Value + value2.Value;
+            }
         }
     }
 }
