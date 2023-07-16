bb9dc89014e86d2340026b4a6a4a45154e54f9c6
Matuyuhi <bird9.yuhi@gmail.com>
2023-07-16 16:25:07 +0900

    fadeOutできない問題の修正

--- Scripts/RunTime/Fade.cs
+++ Scripts/RunTime/Fade.cs
@@ -4,25 +4,24 @@ namespace AnimationPro.RunTime
 {
     internal class FadeImpl : ContentTransform
     {
-        private float beforeRatio;
         private float lastFrameRatio;
+        private bool isIn;
 
         public FadeImpl(AnimationSpec a, bool isIn) : base(a)
         {
-            beforeRatio = isIn ? 1f : 0f;
-            lastFrameRatio = beforeRatio;
+            this.isIn = isIn;
         }
 
         public override TransitionSpec OnUpdate(float frame)
         {
             var currentFrameRatio = AnimationSpec.GetRatio(frame);
             var diff = currentFrameRatio - lastFrameRatio;
-        
-            // 今回の値を記憶しておく
+
+            // Store the current value for the next frame
             lastFrameRatio = currentFrameRatio;
 
-            // 差分を返す
-            return new TransitionSpec(alpha: diff);
+            // Return the difference
+            return new TransitionSpec(alpha: isIn ? diff : -diff);
         }
     }
 }
